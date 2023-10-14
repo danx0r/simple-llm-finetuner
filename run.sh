127ac3182cd610b540a693c8d3812a5228c04a6d
Ubuntu <ubuntu@ip-172-31-40-1.us-west-2.compute.internal>
2023-10-14 20:21:28 +0000

    run.sh, add scipy to reqs

---
+++ run.sh
@@ -0,0 +1,9 @@
+#Notebook:
+#
+#![[ -d /content/simple-llama-finetuner ]] \
+#  || git clone https://github.com/lxe/simple-llama-finetuner.git /content/simple-llama-finetuner
+#!cd /content/simple-llama-finetuner && git pull && pip install -r requirements.txt
+#
+#!cd /content/simple-llama-finetuner && python app.py --share
+
+python app.py --share
