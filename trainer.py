e560780fd453e2da13e1158c9845df63024be10b
danx0r <dan.miller@eye0.com>
2023-10-18 21:02:30 +0000

    test.sh working somewhat

--- trainer.py
+++ trainer.py
@@ -295,7 +295,7 @@ if __name__ == '__main__':
     prompt = "Human: How is cheese made?\n\nAssistant:"
     print(t.generate(prompt))
 
-    t.load_lora(LORA_TRAINING_PARAMS['lora'])
+    t.load_lora(LORA_TRAINING_PARAMS['lora_name'])
     print(t.generate(prompt))
 
     t.unload_lora()
