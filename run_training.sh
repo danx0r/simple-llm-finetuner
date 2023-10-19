af707ea74ca1c651f6938710dbd079d95c970a50
danx0r <danbmil99@gmail.com>
2023-10-19 01:00:06 -0700

    rm dead param

--- run_training.sh
+++ run_training.sh
@@ -1,1 +1,1 @@
-python trainer.py --run_training --model "decapoda-research/llama-7b-hf" --lora "lora/decapoda-research_llama-7b-hf_unhelp"
+python trainer.py --run_training --model "decapoda-research/llama-7b-hf"
